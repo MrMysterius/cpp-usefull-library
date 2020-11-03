eb0dc80e3dec932ed9cb0b1730a0cb625d62ff6b
MrMysterius <official.mrmysterius@gmail.com>
2020-11-03 14:37:02 +0100

    Init/First Commit

---
+++ Usefull Library.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//JUST PRINTS A LINE
+void line(char character, int length, bool newLines) {
+	if (newLines) cout << "\n";
+	for (int i = 0; i < length; i++) {
+		cout << character;
+	}
+	if (newLines) cout << "\n\n";
+}
+
+//INPUTS
+int getInt(string name, int min, int max) {
+    int input = min-1;
+    bool isFinished = false;
+
+    cout << "\nEnter " << name << " between " << min << " and " << max << "!\n";
+
+    do {
+        cout << ": ";
+        cin >> input;
+
+        if (cin.fail()) {
+            cin.clear();
+            cout << "ERROR: Input Error\n";
+        }
+
+        cin.ignore(1000000, '\n');
+
+        if ((min == 0 && max == 0) || (input >= min && (input <= max || max == 0))) {
+            isFinished = true;
+        }
+        else {
+            cout << "ERROR: Input not in allowed number range\n";
+        }
+
+    } while (!isFinished);
+
+    return input;
+}
+
+double getDouble(string name, double min, double max) {
+    double input = min - 1;
+    bool isFinished = false;
+
+    cout << "\nEnter " << name << " between " << min << " and " << max << "!\n";
+
+    do {
+        cout << ": ";
+        cin >> input;
+
+        if (cin.fail()) {
+            cin.clear();
+            cout << "ERROR: Input Error\n";
+        }
+
+        cin.ignore(1000000, '\n');
+
+        if ((min == 0 && max == 0) || (input >= min && (input <= max || max == 0))) {
+            isFinished = true;
+        }
+        else {
+            cout << "ERROR: Input not in allowed number range\n";
+        }
+
+    } while (!isFinished);
+
+    return input;
+}
+
+char getChar(string name) {
+    char input;
+    bool isFinished = false;
+
+    cout << "\nEnter " << name << "!\n";
+
+    do {
+        cout << ": ";
+        cin >> input;
+
+        isFinished = true;
+        if (cin.fail()) {
+            cin.clear();
+            cout << "ERROR: Input Error\n";
+            isFinished = false;
+        }
+
+        cin.ignore(1000000, '\n');
+    } while (!isFinished);
+
+    return input;
+}
+
+string getString(string name, int minLength, int maxLength) {
+    string input;
+    bool isFinished = false;
+
+    cout << "\nEnter " << name << " (Min: " << minLength << " Max: " << maxLength << ")!\n";
+
+    do {
+        cout << ": ";
+        cin >> input;
+
+        if (cin.fail()) {
+            cin.clear();
+            cout << "ERROR: Input Error\n";
+        }
+
+        cin.ignore(1000000, '\n');
+
+        if ((minLength == 0 && maxLength == 0) || (input.length >= minLength && (maxLength == 0 || input.length <= maxLength))) {
+            isFinished = true;
+        }
+        else {
+            cout << "ERROR: Input not in allowed range\n";
+        }
+    } while (!isFinished);
+
+    return input;
+}
---
+++ Usefull Library.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+//FORMATING
+void line(char character = '_', int length = 80, bool newLines = true);
+
+//INPUTS
+int getInt(std::string name = "an integer", int min = 0, int max = 0);
+double getDouble(std::string name = "an double", double min = 0.0, double max = 0.0);
+char getChar(std::string name = "an character");
+std::string getString(std::string name = "an string", int minLength = 0, int maxLength = 0);
